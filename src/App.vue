d4ed6ec83304df35c3b85ea14afadeaab9d34606
bibichuan <741841403@qq.com>
2019-05-09 09:33:39 +0800

    禁止选中

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  -webkit-touch-callout: none;
+  -moz-user-select: none; /*火狐*/
+  -webkit-user-select: none;  /*webkit浏览器*/
+  -ms-user-select: none;   /*IE10*/
+  -khtml-user-select: none; /*早期浏览器*/
+  user-select: none;
 }
 </style>
